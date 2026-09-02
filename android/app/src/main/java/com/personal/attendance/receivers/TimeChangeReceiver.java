b859206ac4c4ed14844ae4e7e61ad3892c511f0e
moustafsaeed <moustafamaqtari@gmail.com>
2026-09-02 20:13:55 +0300

    Fix

--- android/app/src/main/java/com/personal/attendance/receivers/TimeChangeReceiver.java
+++ android/app/src/main/java/com/personal/attendance/receivers/TimeChangeReceiver.java
@@ -6,19 +6,15 @@ import android.content.Intent;
 import android.util.Log;
 import com.personal.attendance.notifications.NotificationScheduler;
 
-import static android.content.Intent.ACTION_TIME_SET;
-import static android.content.Intent.ACTION_TIMEZONE_CHANGED;
-import static android.content.Intent.ACTION_DATE_CHANGED;
-
 public class TimeChangeReceiver extends BroadcastReceiver {
     private static final String TAG = "TIME_CHANGE_RECEIVER";
 
     @Override
     public void onReceive(Context context, Intent receivedIntent) {
         String action = receivedIntent.getAction();
-        if (ACTION_TIME_SET.equals(action) || 
-            ACTION_TIMEZONE_CHANGED.equals(action) || 
-            ACTION_DATE_CHANGED.equals(action)) {
+        if (Intent.ACTION_TIME_SET.equals(action) || 
+            Intent.ACTION_TIMEZONE_CHANGED.equals(action) || 
+            Intent.ACTION_DATE_CHANGED.equals(action)) {
             Log.d(TAG, "Time/Timezone changed, rescheduling...");
             NotificationScheduler.rescheduleAll(context);
         }
